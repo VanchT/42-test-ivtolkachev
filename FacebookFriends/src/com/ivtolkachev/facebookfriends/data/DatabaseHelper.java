91561a79b46a8a9ba2b6707fab3f1ff760798894
ivtolkachev <ivtolkachev@gmail.com>
2013-02-21 19:08:52 +0200

    ticket:2 Database was changed. Added 2 tests.

--- FacebookFriends/src/com/ivtolkachev/facebookfriends/data/DatabaseHelper.java
+++ FacebookFriends/src/com/ivtolkachev/facebookfriends/data/DatabaseHelper.java
@@ -19,21 +19,49 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 	private static final String CREATE_TABLE = "CREATE TABLE IF NOT EXISTS ";
 	//Tables names
 	public static final String USERS_TABLE = "Users";
+	public static final String LOCATIONS_TABLE = "Locations";
 	//Table Users
-	public static final String USER_ID = "user_id";
-	public static final String USER_NAME = "user_name";
-	public static final String USER_SURNAME = "user_surname";
-	public static final String USER_BIRTH = "user_birth";
-	public static final String USER_BIO = "user_bio";
-	public static final String USER_CONTACTS = "user_contacts";
+	public static final String USER_ID = "userId";
+	public static final String USER_NAME = "userName";
+	public static final String USER_FIRST_NAME = "userFirstName";
+	public static final String USER_MIDDLE_NAME = "userMiddleName";
+	public static final String USER_LAST_NAME = "userLastName";
+	public static final String USER_USERNAMEE = "userUsername";
+	public static final String USER_BIRTHDAY = "userBirthday";
+	public static final String USER_LINK = "userLink";
+	//Table Users
+	public static final String LOCATION_ID = "locationId";
+	public static final String LOCATION_COUNTRY = "country";
+	public static final String LOCATION_STATE = "state";
+	public static final String LOCATION_CITY = "city";
+	public static final String LOCATION_STREET = "street";
+	public static final String LOCATION_ZIP = "zip";
+	public static final String LOCATION_LATITUDE = "latitude";
+	public static final String LOCATION_LONGITUDE = "longitude";
+	
 	
 	private static final String CREATE_USERS_TABLE_STRING = "(" 
-			+ USER_ID + " integer primary key, " 
-			+ USER_NAME + " text not null, "
-			+ USER_SURNAME + " text, "
-			+ USER_BIRTH + " integer, "
-			+ USER_BIO + " text, "
-			+ USER_CONTACTS + " text"
+			+ USER_ID + " text primary key, " 
+			+ USER_NAME + " text , "
+			+ USER_FIRST_NAME + " text, "
+			+ USER_MIDDLE_NAME + " integer, "
+			+ USER_LAST_NAME + " text, "
+			+ USER_USERNAMEE + " text, "
+			+ USER_BIRTHDAY + " text, "
+			+ USER_LINK + " text"
+			+ ");";
+	
+	private static final String CREATE_LOCATIONS_TABLE_STRING = "(" 
+			+ LOCATION_ID + " integer primary key, " 
+			+ LOCATION_COUNTRY + " text, "
+			+ LOCATION_STATE + " text , "
+			+ LOCATION_CITY + " integer, "
+			+ LOCATION_STREET + " text, "
+			+ LOCATION_ZIP + " text, "
+			+ LOCATION_LATITUDE + " integer, "
+			+ LOCATION_LONGITUDE + " integer, "
+			+ USER_ID + " text, "
+			+ "FOREIGN KEY (" + USER_ID + ") REFERENCES " + USERS_TABLE + " (" + USER_ID + ")"
 			+ ");";
 	
 	public DatabaseHelper(Context context) {
@@ -43,32 +71,13 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 	@Override
 	public void onCreate(SQLiteDatabase db) {
 		db.execSQL(CREATE_TABLE + USERS_TABLE + CREATE_USERS_TABLE_STRING);
-		
-		insertData(db);
+		db.execSQL(CREATE_TABLE + LOCATIONS_TABLE + CREATE_LOCATIONS_TABLE_STRING);
 	}
 
 	@Override
 	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 		// TODO Auto-generated method stub
 
-	}
-	
-	//TODO: This method has created temporally for testing.
-	private void insertData(SQLiteDatabase db){		
-		String birth_date="1988-12-16";
-		SimpleDateFormat formatter ; 
-		Date date = null ; 
-		formatter = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
-		try {
-			date = (Date) formatter.parse(birth_date);
-		} catch (ParseException e) {
-			e.printStackTrace();
-		} 
-		Log.d(TAG, "Today is " + date.getTime());
-		
-		db.execSQL("INSERT INTO " + USERS_TABLE + " VALUES (120, 'Иван', 'Толкачёв', " + date.getTime() 
-				+ ", 'Android developer from Kherson'," 
-				+ " 'email: ivtolkachev@gmail.com;skype: john_pilgrim_midnight_warrior;jabber: ivtolkachev@jabber.org;phone: +380950731193')");
 	}
 
 }
--- FacebookFriends/test/com/ivtolkachev/facebookfriends/test/DatabaseWorkerTest.java
+++ FacebookFriends/test/com/ivtolkachev/facebookfriends/test/DatabaseWorkerTest.java
@@ -3,16 +3,19 @@ package com.ivtolkachev.facebookfriends.test;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
+import java.util.Map;
+
+import org.json.JSONObject;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
 
-import android.app.Activity;
 import android.content.SharedPreferences;
 import android.database.sqlite.SQLiteDatabase;
-import android.util.Log;
 
+import com.facebook.model.GraphLocation;
+import com.facebook.model.GraphObject;
 import com.ivtolkachev.facebookfriends.R;
 import com.ivtolkachev.facebookfriends.activity.MainActivity;
 import com.ivtolkachev.facebookfriends.data.DatabaseWorker;
@@ -55,9 +58,25 @@ public class DatabaseWorkerTest {
 		assertTrue(testId == user.getUserId());
 	}
 	
+	@Test
+	public void testGetUserLocation() throws Exception {
+		GraphLocation location = null;
+		long testId = mAppPref.getLong(mActivity.getString(R.string.preference_user_id), -1);
+		assertTrue(testId > -1);
+		location = mDatabaseWorker.getUserLocation(testId);
+		assertNotNull(location);
+	}
+	
 	@Test 
-	public void testAddUser() throws Exception {
+ 	public void testAddUser() throws Exception {
 		User user = new User(111, "Ivan", "Ivanov", System.currentTimeMillis(), "Java developer", new String[] {"phone: +3809523432312"}); 
+		long rowId = mDatabaseWorker.addUser(user);
+		assertTrue(rowId > -1);
+	}
+	
+	@Test 
+ 	public void testAddLocation() throws Exception {
+		UserLocation location = new UserLocation("Ukraine", null, "Kherson", "Dimitrova", "73020", null, null, 112);
 		long rowId = mDatabaseWorker.addUser(user);
 		assertTrue(rowId > -1);
 	}
